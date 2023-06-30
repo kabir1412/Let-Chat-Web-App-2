97729d8a60dee00a57f195e1984cc35cf7792935
kabir1412 <87866985+kabir1412@users.noreply.github.com>
2023-06-30 20:30:42 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCERdVrF3QaqLPN_h8xzrAEr7V3-gdcBvQ",
+    authDomain: "let-chat-web-app-3875f.firebaseapp.com",
+    databaseURL: "https://let-chat-web-app-3875f-default-rtdb.firebaseio.com",
+    projectId: "let-chat-web-app-3875f",
+    storageBucket: "let-chat-web-app-3875f.appspot.com",
+    messagingSenderId: "424095145183",
+    appId: "1:424095145183:web:0a283dc973f738af749d6a"
+  };
